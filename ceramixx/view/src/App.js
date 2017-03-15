412b04171a3f8c11efd8c63bd654e62dbaedf10e
gdenn <gdennis91@googlemail.com>
2017-03-15 14:34:10 +0100

    Add redux

--- ceramixx/view/src/App.js
+++ ceramixx/view/src/App.js
@@ -6,13 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron</h2>
-        </div>
-        <p className="App-intro">
-          Hello Electron!
-        </p>
+      	{this.props.children}
       </div>
     );
   }
--- ceramixx/view/src/index.js
+++ ceramixx/view/src/index.js
@@ -1,9 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore, compose } from 'redux'
+import createLogger from 'redux-logger'
+import { Provider } from 'react-redux'
+import {Route, IndexRoute} from 'react-router';
+
+import { Router, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+
+// Components used by the router
 import App from './App';
-import './index.css';
+
+//import reducers from './reducers'
+import allReducers from './reducers/index.js'
+
+// Add bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+// logger for development modus @see redux-logger
+const logger = createLogger()
+// This implements Redux dev tools into our project
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// create store and register all available reducers
+let store = createStore(
+  allReducers, composeEnhancers(
+  applyMiddleware(logger)
+  )
+);
+
+store.subscribe(() => {
+  //not needed anymore as we use the logger now
+  //console.log("store changed", store.getState());
+});
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
+
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    { /* Tell the Router to use our enhanced history */ }
+    <Router history={history}>
+      <Route path="/" component={App}>
+
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
