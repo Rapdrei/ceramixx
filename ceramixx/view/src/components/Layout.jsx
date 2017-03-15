7686e6de15197900770d939fb5cc046753cd6da8
gdenn <gdennis91@googlemail.com>
2017-03-15 21:23:22 +0100

    Add bootstrap to setup

---
+++ ceramixx/view/src/components/Layout.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export const Layout = () => (
+  <div>
+    <p>hello world</p>
+  </div>
+);
--- ceramixx/view/src/index.js
+++ ceramixx/view/src/index.js
@@ -10,6 +10,7 @@ import { syncHistoryWithStore } from 'react-router-redux'
 
 // Components used by the router
 import App from './App';
+import Layout from './components/Layout';
 
 //import reducers from './reducers'
 import allReducers from './reducers/index.js'
@@ -38,14 +39,11 @@ store.subscribe(() => {
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store)
 
-
-
 ReactDOM.render(
   <Provider store={store}>
     { /* Tell the Router to use our enhanced history */ }
     <Router history={history}>
       <Route path="/" component={App}>
-
       </Route>
     </Router>
   </Provider>,
