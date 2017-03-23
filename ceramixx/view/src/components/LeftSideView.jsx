cfc5ea6d2faafa7d93c417e43155001b0b97fd92
gdenn <gdennis91@googlemail.com>
2017-03-23 16:58:48 +0100

    Pull Settings into LeftSideView state

--- ceramixx/view/src/components/LeftSideView.jsx
+++ ceramixx/view/src/components/LeftSideView.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react'
 
 import SettingsMenu from './SettingsMenu'
+import WorkspaceSettings from './WorkspaceSettings'
 
 export default class LeftSideView extends React.Component {
+  constructor(...props) {
+    super(...props)
+
+    this.state = {
+      WorkspaceSettings: {
+
+      },
+      PlacementSettings: {
+
+      },
+      CustomCodeSettings: {
+
+      },
+      SlicerSettings: {
+
+      }
+    }
+  }
+
   render() {
     return (
       <div className="left-view">
@@ -10,8 +30,19 @@ export default class LeftSideView extends React.Component {
           <p className="logo-title"> cerami<span>x</span><span>x</span></p>
         </div>
         <SettingsMenu/>
-
+        <WorkspaceSettings/>
+        <
       </div>
     )
   }
-} 
+
+  redirectSettingsView(view) {
+    switch (view) {
+      case 'expression':
+
+        break;
+      default:
+
+    }
+  }
+}
